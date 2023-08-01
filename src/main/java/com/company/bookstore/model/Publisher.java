0316195c22cff6f9d4eb2faef480d13948afe566
angelouis <alouis001@citymail.cuny.edu>
2023-08-01 19:48:31 -0400

    added foreign key

--- src/main/java/com/company/bookstore/model/Publisher.java
+++ src/main/java/com/company/bookstore/model/Publisher.java
@@ -3,6 +3,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -27,6 +28,10 @@ public class Publisher implements Serializable {
     private String phone;
 
     private String email;
+
+   // @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch= FetchType.EAGER)
+    //@JoinColumn(name="publisher_id")
+    //private List<Book> books;
 
     public Publisher() {
     }
