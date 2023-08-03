8801611b10aacb43e4cc93c69d2e09467257b7e1
alexi <aibasita@uci.edu>
2023-08-03 18:00:43 -0400

    test

--- src/main/java/com/company/bookstore/repository/PublisherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.bookstore.repository;
-
-import com.company.bookstore.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PublisherRepository extends JpaRepository<Book,Integer> {
-}
